939e7f52c6d5db9123f757b19726ba74dd6d38ee
maejimafumika <maejima-fumika075@g.ecc.u-tokyo.ac.jp>
2025-03-31 20:15:55 +0900

    Fix module transpiler.

--- server/src/tools/transpile-module.ts
+++ server/src/tools/transpile-module.ts
@@ -1,8 +1,6 @@
 import { transpile } from '../transpiler/code-generator/code-generator'
 import * as fs from 'fs'
-import { GlobalVariableNameTable } from '../transpiler/code-generator/variables'
 import { FILE_PATH } from '../constants'
-import Session from "../server/session";
 
 
 const cProlog = `
@@ -11,11 +9,20 @@ const cProlog = `
 #include "../${FILE_PATH.PROFILER_H}"
 `
 
+function readModuleId(moduleName: string) {
+  const moduleIds: {[name: string]: string} = JSON.parse(fs.readFileSync(FILE_PATH.MODULE_NAME_TO_ID).toString());
+  const moduleId = moduleIds[moduleName];
+    if (moduleId === undefined) {
+      throw Error(`Cannot find module id corresponding to module name: ${moduleName}`);
+    }
+    return moduleId;
+}
+
 function main(moduleName: string) {
-  const src = fs.readFileSync(`${FILE_PATH.MODULES}/${moduleName}/${moduleName}.bs`).toString()
-  const result = transpile(0, src, undefined, undefined, Session.moduleNameToId(moduleName)).code
-  fs.writeFileSync(`${FILE_PATH.MODULES}/${moduleName}/${moduleName}.c`, cProlog + result)
-  console.log('done')
+  const src = fs.readFileSync(`${FILE_PATH.MODULES}/${moduleName}/${moduleName}.bs`).toString();
+  const result = transpile(0, src, undefined, undefined, readModuleId(moduleName)).code;
+  fs.writeFileSync(`${FILE_PATH.MODULES}/${moduleName}/${moduleName}.c`, cProlog + result);
+  console.log('done');
 }
 
 main(process.argv[2]);
