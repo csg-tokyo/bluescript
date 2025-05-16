c87902235d9bbe0c77bb5f5ee26b5447b0f57ba8
maejimafumika <maejima-fumika075@g.ecc.u-tokyo.ac.jp>
2025-05-16 15:37:32 +0900

    Fix microcontroller to link every core function.

--- microcontroller/core/include/main-thread.h
+++ microcontroller/core/include/main-thread.h
@@ -4,14 +4,16 @@
 #include <stdint.h>
 #include "c-runtime.h"
 
-void bs_main_thread_init();
+#define CORE_TEXT_SECTION __attribute__((section(".core_text")))
 
-void bs_main_thread_reset();
+void CORE_TEXT_SECTION bs_main_thread_init();
 
-void bs_main_thread_set_main(int32_t id, void* address);
+void CORE_TEXT_SECTION bs_main_thread_reset();
 
-void bs_main_thread_set_event(value_t fn);
+void CORE_TEXT_SECTION bs_main_thread_set_main(int32_t id, void* address);
 
-void bs_main_thread_set_event_from_isr(void* fn);
+void CORE_TEXT_SECTION bs_main_thread_set_event(value_t fn);
+
+void CORE_TEXT_SECTION bs_main_thread_set_event_from_isr(void* fn);
 
 #endif /* __BS_MAIN_THREAD__ */
--- microcontroller/ports/esp32/main/memory.c
+++ microcontroller/ports/esp32/main/memory.c
@@ -44,12 +44,14 @@ static void iflash_init() {
     iflash_partition = esp_partition_find_first(ESP_PARTITION_TYPE_DATA, ESP_PARTITION_SUBTYPE_ANY, IFLASH_PARTITION_LABEL);
     esp_partition_mmap(iflash_partition, 0, iflash_partition->size, ESP_PARTITION_MMAP_INST, &mapped_iflash_address, &mapped_iflash_hdlr);
     BS_LOG_INFO("IFlash Address: 0x%x Mapped Address: %p Size: %d\n", (int)iflash_partition->address, mapped_iflash_address, (int)iflash_partition->size)
+    esp_partition_erase_range(iflash_partition, 0, iflash_partition->size);
 }
 
 static void dflash_init() {
     dflash_partition = esp_partition_find_first(ESP_PARTITION_TYPE_DATA, ESP_PARTITION_SUBTYPE_ANY, DFLASH_PARTITION_LABEL);
     esp_partition_mmap(dflash_partition, 0, dflash_partition->size, ESP_PARTITION_MMAP_DATA, &mapped_dflash_address, &mapped_dflash_hdlr);
     BS_LOG_INFO("DFlash Address: 0x%x VAddress: %p Size: %d\n", (int)dflash_partition->address, mapped_dflash_address, (int)dflash_partition->size)
+    esp_partition_erase_range(dflash_partition, 0, dflash_partition->size);
 }
 
 static void iram_reset() {
