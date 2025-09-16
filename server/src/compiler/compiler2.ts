d0971a323aedf46869a4952dc9677c6c2f7214f6
maejimafumika <maejima-fumika075@g.ecc.u-tokyo.ac.jp>
2025-09-16 14:53:37 +0900

    Update package system.

---
+++ server/src/compiler/compiler2.ts
@@ -0,0 +1,350 @@
+import { GlobalVariableNameTable } from "../transpiler/code-generator/variables";
+import { transpile } from "../transpiler/code-generator/code-generator";
+import { FILE_PATH } from "../constants";
+import * as fs from "fs";
+import * as path from "path";
+import {Buffer} from "node:buffer";
+import { spawn } from "child_process";
+import generateLinkerScript from "./linkerscript2";
+import generateMakefile from "./makefile";
+import { ElfReader } from "./elf-reader";
+
+
+export type PackageConfig = {
+    name: string,
+    espIdfComponents: string[],
+    dependencies: string[],
+    dirs: {
+        src: string,
+        dist: string,
+        build: string
+    }
+}
+
+export type CompilerConfig = {
+    dirs: {
+        packages: string,
+        runtime: string,
+        compileToolchain: string
+        std: string
+    }
+};
+
+export type MemoryLayout = {
+    iram:{address:number, size:number},
+    dram:{address:number, size:number},
+    iflash:{address:number, size:number},
+    dflash:{address:number, size:number},
+}
+
+
+type MemoryRegion = {name: string, address: number, size: number}
+
+export class ShadowMemory {
+    public iram: MemoryRegion;
+    public dram: MemoryRegion;
+    public iflash: MemoryRegion;
+    public dflash: MemoryRegion;
+
+    constructor(memoryLayout: MemoryLayout) {
+        this.iram = {name: 'IRAM', ...memoryLayout.iram};
+        this.dram = {name: 'DRAM', ...memoryLayout.dram};
+        this.iflash = {name: 'IFlash', ...memoryLayout.iflash};
+        this.dflash = {name: 'DFlash', ...memoryLayout.dflash};
+    }
+}
+
+export type ExecutableBinary = {
+    iram: {address: number, data: Buffer},
+    dram: {address: number, data: Buffer},
+    iflash: {address: number, data: Buffer},
+    dflash: {address: number, data: Buffer},
+    entryPoints: {id: number, address: number}[]
+}
+
+const MODULE_PATH = (pkg: PackageConfig, relativePath: string) => path.normalize(path.join(pkg.dirs.src, `${relativePath}.bs`));
+const MODULE_C_PATH = (dir: string, moduleName: string) => path.join(dir, `bs_${moduleName}.c`);
+const ARCHIVE_PATH = (pkg: PackageConfig) => path.join(pkg.dirs.build, `lib${pkg.name}.a`);
+const MAKEFILE_PATH = (pkg: PackageConfig) => path.join(pkg.dirs.dist, 'Makefile');
+const RUNTIME_ELF_PATH = (runtimeDir: string) => path.join(runtimeDir, 'ports/esp32/build/bluescript.elf');
+const LINKER_SCRIPT = (pkg: PackageConfig) => path.join(pkg.dirs.build, "linkerscript.ld");
+const LD_PATH = (toolChainDir: string) => path.join(toolChainDir, 'xtensa-esp32-elf-ld');
+const LINKED_ELF_PATH = (pkg: PackageConfig) => path.join(pkg.dirs.build, `${pkg.name}.elf`);
+
+
+class Compiler {
+    private config: CompilerConfig;
+    private espidfComponents: ESPIDFComponents;
+    private memory: ShadowMemory;
+    private packageReader: (name: string) => PackageConfig;
+    private logger: {
+            info: (message: string) => void,
+            error: (message: string) => void
+    };
+
+    constructor(
+        memoryLayout: MemoryLayout, 
+        config: CompilerConfig, 
+        packageReader: (name: string) => PackageConfig,
+        logger: {
+            info: (message: string) => void,
+            error: (message: string) => void
+        }
+    ) {
+        this.config = config;
+        this.espidfComponents = new ESPIDFComponents(config.dirs.runtime);
+        this.memory = new ShadowMemory(memoryLayout);
+        this.packageReader = packageReader;
+        this.logger = logger;
+    }
+
+    public async compile(): Promise<ExecutableBinary> {
+        const {mainPackage, subPackages, mainEntryPoint, subModuleEntryPoints} = this._transpile();
+        await this._compile(mainPackage, subPackages);
+        await this._link(mainPackage, subPackages, mainEntryPoint);
+        return this._generateExecutableBinary(mainPackage, mainEntryPoint, subModuleEntryPoints);
+    }
+
+    private _transpile() {
+        let compileId = 0;
+        const stdSrc = fs.readFileSync(path.join(this.config.dirs.std, 'index.bs'), 'utf-8');
+        const nameTable = transpile(++compileId, stdSrc, undefined).names;
+        const modules = new Map<string, GlobalVariableNameTable>(); // <path, nameTable>
+        const subModuleEntryPoints: string[] = []
+
+        const mp = this.packageReader('main');
+        if (mp === undefined) { throw new Error('Could not find main package.'); }
+        const mainPackage: PackageConfig = mp;
+        const visitedPackages: PackageConfig[] = [];
+        let currentPackage: PackageConfig = mainPackage;
+
+        const importer = (fname: string) => {
+            let relativePath: string;
+            if (!fname.startsWith('.')) {
+                let pkg = visitedPackages.find(p => p.name === fname)
+                if (!pkg) { // not visited package
+                    currentPackage = this.packageReader(fname);
+                    visitedPackages.push(currentPackage);
+                }
+                relativePath = './index';
+            } else {
+                relativePath = fname;
+            }
+
+            const modulePath = MODULE_PATH(currentPackage, relativePath);
+            const mod = modules.get(modulePath);
+            if (mod)
+                return mod;
+            else {
+                if (!fs.existsSync(modulePath)) {
+                    throw new Error(`Cannot find ${modulePath}`);
+                }
+                // Transpile
+                const src = fs.readFileSync(modulePath).toString();
+                const id = ++compileId;
+                const result = transpile(id, src, nameTable, importer, id);
+                modules.set(modulePath, result.names);
+                subModuleEntryPoints.push(result.main);
+                // Save C code
+                const cSaveDir = path.join(currentPackage.dirs.dist, path.parse(relativePath).base);
+                fs.mkdirSync(cSaveDir, {recursive: true});
+                fs.writeFileSync(MODULE_C_PATH(cSaveDir, path.parse(relativePath).name), result.code);
+                return result.names;
+            }
+        }
+    
+        try {
+            this.logger.info('Transpiling...');
+            const mainSrc = fs.readFileSync(MODULE_PATH(mainPackage, './index'), 'utf-8');
+            const result = transpile(++compileId, mainSrc, nameTable, importer);
+            fs.writeFileSync(MODULE_C_PATH(mainPackage.dirs.dist, 'index'), result.code);
+            return {mainPackage, subPackages: visitedPackages, mainEntryPoint: result.main, subModuleEntryPoints};
+        } catch (error) {
+            this.logger.error(`Failed to transpile: ${error}`);
+            throw error;
+        }  
+    }
+
+    private async _compile(mainPackage: PackageConfig, subPackages: PackageConfig[]) {
+        this.logger.info('Compiling with GCC...');
+
+        // Compile each package with make.
+        const commonIncludeDirs = this.espidfComponents.commonIncludeDirs;
+        for (const pkg of [mainPackage, ...subPackages]) {
+            try {
+                this.logger.info(`Compiling ${pkg.name}...`);
+                const makefile = generateMakefile(
+                    this.config.dirs.compileToolchain,
+                    pkg,
+                    [...this.espidfComponents.getIncludeDirs(pkg.dependencies), ...commonIncludeDirs],
+                    ARCHIVE_PATH(pkg)
+                );
+                fs.writeFileSync(MAKEFILE_PATH(pkg), makefile);
+                await executeCommand('make', pkg.dirs.dist);
+            } catch (error) {
+                this.logger.error(`Failed to compile ${pkg.name}: ${error}`);
+                throw error;
+            }
+        }
+    }
+
+    private async _link(mainPackage: PackageConfig, subPackages: PackageConfig[], mainEntryPoint: string) {
+        this.logger.info(`Linking...`);
+        try {
+            const elfReader = new ElfReader(RUNTIME_ELF_PATH(this.config.dirs.runtime));
+            const symbolsInRuntime = elfReader.readAllSymbols().map(symbol => ({name: symbol.name, address: symbol.address}));
+            const linkerscript = generateLinkerScript(
+                ARCHIVE_PATH(mainPackage),
+                this._getArchives(mainPackage, subPackages),
+                this.memory,
+                symbolsInRuntime,
+                mainEntryPoint
+            );
+            fs.writeFileSync(LINKER_SCRIPT(mainPackage), linkerscript);
+            await executeCommand(`${LD_PATH(this.config.dirs.compileToolchain)} -o ${LINKED_ELF_PATH(mainPackage)} -T ${LINKER_SCRIPT(mainPackage)}`);
+        } catch (error) {
+            this.logger.error(`Failed to link: ${error}`);
+            throw error;
+        }
+    }
+
+    private _getArchives(mainPackage: PackageConfig, subPackages: PackageConfig[]): string[] {
+        const espArchivesFromMain = this.espidfComponents.getArchiveFilePaths(mainPackage.espIdfComponents);
+        const resultArchives = [ARCHIVE_PATH(mainPackage), ...espArchivesFromMain];
+        const visitedEspArchives = new Set(espArchivesFromMain);
+        const reversedPackages = subPackages.reverse();
+        const addEspArchive = (espArchive: string) => {
+            if (!visitedEspArchives.has(espArchive)) {
+                resultArchives.push(espArchive);
+                visitedEspArchives.add(espArchive);
+            }
+        }
+        for (const pkg of reversedPackages) {
+            resultArchives.push(ARCHIVE_PATH(pkg));
+            const espArchivesFromPkg = this.espidfComponents.getArchiveFilePaths(pkg.espIdfComponents);
+            espArchivesFromPkg.forEach(ar => addEspArchive(ar));
+        }
+        this.espidfComponents.commonArchiveFilePaths.forEach(ar => addEspArchive(ar));
+        return resultArchives;
+    }
+
+    private _generateExecutableBinary(
+        mainPackage: PackageConfig, mainEntryPointName: string, subEntryPointNames: string[]
+    ): ExecutableBinary {
+        const linkedElf = new ElfReader(LINKED_ELF_PATH(mainPackage));
+        const definedSymbols = linkedElf.readDefinedSymbols();
+        const iramSection = linkedElf.readSectionByName(this.memory.iram.name);
+        const dramSection = linkedElf.readSectionByName(this.memory.dram.name);
+        const iflashSection = linkedElf.readSectionByName(this.memory.iflash.name);
+        const dflashSection = linkedElf.readSectionByName(this.memory.dflash.name);
+        const entryPoints: {id: number, address: number}[] = [];
+        const setEntryPoint = (id: number, name: string) => {
+            const symbol = definedSymbols.find(s => s.name === name);
+            if (symbol) { entryPoints.push({id, address: symbol.address}); }
+            else { throw new Error(`Cannot find ${name} in executable elf.`) }
+        }
+        subEntryPointNames.forEach(epn => setEntryPoint(-1, epn));
+        setEntryPoint(0, mainEntryPointName);
+        const emptyBuffer = Buffer.from([]);
+        return {
+            iram: {address: this.memory.iram.address, data: iramSection?.value ?? emptyBuffer},
+            dram: {address: this.memory.dram.address, data: dramSection?.value ?? emptyBuffer},
+            iflash: {address: this.memory.iflash.address, data: iflashSection?.value ?? emptyBuffer},
+            dflash: {address: this.memory.dflash.address, data: dflashSection?.value ?? emptyBuffer},
+            entryPoints
+        }
+    }
+}
+
+
+class ESPIDFComponents {
+    public readonly commonIncludeDirs: string[];
+    public readonly commonArchiveFilePaths: string[];
+
+    private readonly COMPONENTS_PATH_PREFIX = /^.*microcontroller/;
+    private readonly COMMON_COMPONENTS = ["cxx", "newlib", "freertos", "esp_hw_support", "heap", "log", "soc", "hal", "esp_rom", "esp_common", "esp_system"];
+    private readonly RUNTIME_DIR: string; 
+
+    private dependenciesInfo: {
+        [key: string]: {
+            file: string,
+            dir: string,
+            reqs: string[], 
+            priv_reqs: string[],
+            include_dirs: string[]
+    }};
+
+    constructor(runtimeDir: string) {
+        this.RUNTIME_DIR = runtimeDir;
+        const dependenciesFile = FILE_PATH.DEPENDENCIES_FILE(runtimeDir);
+        this.dependenciesInfo = JSON.parse(fs.readFileSync(dependenciesFile).toString()).build_component_info;
+        this.commonIncludeDirs = this.getIncludeDirs(this.COMMON_COMPONENTS);
+        this.commonArchiveFilePaths = this.getArchiveFilePaths(this.COMMON_COMPONENTS);
+    }
+
+    public getIncludeDirs(rootComponentNames: string[]) {
+        const components = this._getComponents(rootComponentNames);
+        const includeDirs:string[] = [];
+        for (const component of components) {
+            for (const dir of component.include_dirs) {
+                includeDirs.push(path.join(component.dir, dir));
+            }
+        }
+        return includeDirs;
+    }
+
+    public getArchiveFilePaths(rootComponentNames: string[]) {
+        return this._getComponents(rootComponentNames).map(c => c.file);
+    }
+
+    private _getComponents(rootComponentNames: string[]) {
+        let tmp = [...rootComponentNames];
+        let visited = new Set<string>();
+        const components = [];
+        while(tmp.length > 0) {
+        let curr = tmp.shift() as string;
+        visited.add(curr)
+        tmp = tmp.concat(
+            this.dependenciesInfo[curr].priv_reqs.filter((r:string) => !visited.has(r)),
+            this.dependenciesInfo[curr].reqs.filter((r:string) => !visited.has(r))
+        );
+        if (this.dependenciesInfo[curr].file !== undefined && this.dependenciesInfo[curr].file !== '')
+            components.push(this.dependenciesInfo[curr]);
+        }
+        return components;
+    }
+
+    private _convertRuntimeDirPath(absolutePath: string) {
+        return absolutePath.replace(this.COMPONENTS_PATH_PREFIX, this.RUNTIME_DIR);
+    }
+}
+
+
+function executeCommand(command: string, cwd?: string): Promise<void> {
+  return new Promise((resolve, reject) => {
+    const executeProcess = spawn(command, {shell: true, cwd});
+
+    executeProcess.stdout.on('data', (data) => {
+      process.stdout.write(data.toString());
+    });
+
+    executeProcess.stderr.on('data', (data) => {
+      process.stderr.write(data.toString());
+    });
+
+    executeProcess.on('error', (err) => {
+      reject(new Error(`Failed to setup process: ${err.message}`));
+    });
+
+    executeProcess.on('close', (code) => {
+      if (code === 0) {
+        resolve();
+      } else {
+        reject(new Error(`Failed to execute ${command}. Code: ${code}`));
+      }
+    });
+  });
+}
+
+
+
---
+++ server/src/compiler/linkerscript2.ts
@@ -0,0 +1,179 @@
+import { MemoryLayout, ShadowMemory } from "./compiler2";
+
+
+export default function generateLinkerScript(
+    targetFile: string,
+    inputFiles: string[], 
+    shadowMemory: ShadowMemory, 
+    externalSymbols: {name: string, address: number}[],
+    entryPointName: string
+): string {
+    const iramMemory = new MemoryRegion(
+      shadowMemory.iram.name, 
+      [new MemoryAttribute('executable'), new MemoryAttribute('allocatable')], 
+      shadowMemory.iram.address, 
+      1000000
+    );
+    const dramMemory = new MemoryRegion(
+      shadowMemory.dram.name, 
+      [new MemoryAttribute('read/write'), new MemoryAttribute('allocatable')], 
+      shadowMemory.dram.address, 
+      1000000
+    );
+    const iflashMemory = new MemoryRegion(
+      shadowMemory.iflash.name, 
+      [new MemoryAttribute('executable')], 
+      shadowMemory.iflash.address, 
+      1000000
+    );
+    const dflashMemory = new MemoryRegion(
+      shadowMemory.dflash.name, 
+      [new MemoryAttribute('readonly')], 
+      shadowMemory.dflash.address, 
+      1000000
+    );
+    const externalMemory = new MemoryRegion('EXTERNAL', [new MemoryAttribute('executable')], 0, 0);
+
+    const iramSection = new Section('.iram', iramMemory)
+        .section(targetFile, ['.iram*'], true)
+        .align(4);
+    const dramSection = new Section('.dram', dramMemory)
+        .section(targetFile, ['.data', '.data.*', '.bss', '.bss.*', '.dram*'], true)
+    const iflashSection = new Section('.iflash', iflashMemory)
+        .section(targetFile, ['.literal', '.text', '.literal.*', '.text.*'], true)
+        .align(4);
+    const dflashSection = new Section('.dflash', dflashMemory)
+        .section(targetFile, ['.rodata', '.rodata.*'], true)
+    const externalSection = new Section('.external', externalMemory);
+    for (const sym of externalSymbols) {
+        externalSection.symbol(sym.name, sym.address);
+    }
+
+    return new LinkerScript()
+        .input(inputFiles)
+        .entry(entryPointName)
+        .memory([iramMemory, dramMemory, iflashMemory, dflashMemory, externalMemory])
+        .sections([iramSection, dramSection, iflashSection, dflashSection, externalSection])
+        .toString();
+}
+
+class LinkerScript {
+  private commands: Command[] = [];
+
+  input(files: string[]) { this.commands.push(new Input(files)); return this; }
+  entry(entry: string) { this.commands.push(new Entry(entry)); return this; }
+  sections(sections: Section[]) { this.commands.push(new Sections(sections)); return this; }
+  memory(regions: MemoryRegion[]) { this.commands.push(new Memory(regions)); return this; }
+
+  toString() {
+    return this.commands.map(c => c.toString()).join('\n');
+  }
+}
+
+interface Command {
+  toString():string;
+}
+
+class Input implements Command {
+  constructor(private files: string[]) {}
+
+  toString(): string {
+    return `INPUT(${this.files.join(' ')})`;
+  }
+}
+
+class Entry implements Command {
+  constructor(private entry: string) {}
+
+  toString(): string {
+    return `ENTRY(${this.entry})\n`;
+  }
+}
+
+class Memory implements Command {
+  constructor(private regions: MemoryRegion[]) {}
+
+  toString(): string {
+    return `
+MEMORY {
+${this.regions.map(r => `${r.toString(1)}`).join('\n')}
+}
+`;
+  }
+}
+
+class Sections implements Command {
+  constructor(private sections: Section[]) {}
+
+  toString(): string {
+    return `
+SECTIONS {
+${this.sections.map(s => `${s.toString(1)}`).join('\n\n')}
+}
+`;
+  }
+}
+
+class MemoryRegion implements Command {
+  constructor(private name: string,
+              private attributes: MemoryAttribute[],
+              private address: number,
+              private size: number) {}
+
+  getName() { return this.name; }
+
+  toString(indent: number = 0) {
+    return `${'\t'.repeat(indent)}${this.name}   (${this.attributes.map(a => a.toString()).join('')})   : ORIGIN = 0x${this.address.toString(16)},  LENGTH = ${this.size}`;
+  }
+}
+
+class MemoryAttribute {
+  constructor(private attr: 'readonly' | 'read/write' | 'executable' | 'allocatable') {}
+
+  toString() {
+    switch (this.attr) {
+      case "readonly":
+        return 'R';
+      case "read/write":
+        return 'W';
+      case "executable":
+        return 'X';
+      case 'allocatable':
+        return 'A';
+    }
+  }
+}
+
+
+class Section implements Command {
+  private commands: string[] = [];
+
+  constructor(private name: string, private memory: MemoryRegion) {}
+
+  align(align: number) {
+    this.commands.push(`. = ALIGN(${Math.round(align)});`);
+    return this;
+  }
+
+  symbol(name: string, address: number) {
+    this.commands.push(`${name} = 0x${address.toString(16)};`);
+    return this;
+  }
+
+  section(objFilePath: string, sectionNames: string[], keep: boolean = false) {
+    const command = `${objFilePath}(${sectionNames.join(' ')})`;
+    if (keep)
+      this.commands.push(`KEEP(${command})`);
+    else
+      this.commands.push(command);
+    return this;
+  }
+
+  toString(indent: number = 0) {
+    return `\
+${'\t'.repeat(indent)}${this.name} : {
+${this.commands.map(comm => `${'\t'.repeat(indent + 1)}${comm}`).join('\n')}
+${'\t'.repeat(indent)}} > ${this.memory.getName()}`
+  }
+}
+
