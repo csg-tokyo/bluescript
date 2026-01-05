abd471ad8e6bf4c453fe6e765ca5e9a67e8c6864
maejimafumika <maejima-fumika075@g.ecc.u-tokyo.ac.jp>
2026-01-05 15:30:32 +0900

    Refactor update command.

--- cli/src/commands/board/update.ts
+++ cli/src/commands/board/update.ts
@@ -8,16 +8,39 @@ import * as path from 'path';
 
 
 class UpdateHandler extends CommandHandler {
+    private existingRuntimeDir: string | undefined;
+    private existingEspDir: string | undefined;
+    private tmpRuntimeDir = path.join(GLOBAL_SETTINGS.BLUESCRIPT_DIR, 'tmp-runtime');
+    private tmpEspDir = path.join(GLOBAL_SETTINGS.BLUESCRIPT_DIR, 'tmp-esp');
+
     constructor() {
         super(false);
     }
 
     async update() {
-        await this.updateRuntime();
-        if (this.globalConfigHandler.isBoardSetup('esp32')) {
-            await this.updateEsp32();
+        try {
+            await this.updateRuntime();
+            if (this.globalConfigHandler.isBoardSetup('esp32')) {
+                await this.updateEsp32();
+            }
+            this.globalConfigHandler.setVersion(GLOBAL_SETTINGS.VM_VERSION);
+        } catch (error) {
+            // Restore
+            if (this.existingRuntimeDir) {
+                fs.moveDir(this.tmpRuntimeDir, this.existingRuntimeDir);
+            }
+            if (this.existingEspDir) {
+                fs.moveDir(this.tmpEspDir, this.existingEspDir);
+            }
+            throw error;
+        } finally {
+            if (fs.exists(this.tmpRuntimeDir)) {
+                fs.removeDir(this.tmpRuntimeDir);
+            }
+            if (fs.exists(this.tmpEspDir)) {
+                fs.removeDir(this.tmpEspDir);
+            }
         }
-        this.globalConfigHandler.setVersion(GLOBAL_SETTINGS.VM_VERSION);
         this.globalConfigHandler.save();
     }
 
@@ -27,8 +50,9 @@ class UpdateHandler extends CommandHandler {
         if (globalConfig.runtimeDir === undefined || globalConfig.version === GLOBAL_SETTINGS.VM_VERSION) {
             throw new SkipStep('not needed', undefined);
         }
+        this.existingRuntimeDir = globalConfig.runtimeDir;
         
-        fs.removeDir(globalConfig.runtimeDir);
+        fs.moveDir(this.existingRuntimeDir, this.tmpRuntimeDir);
         await fs.downloadAndUnzip(GLOBAL_SETTINGS.RUNTIME_ZIP_URL, GLOBAL_SETTINGS.BLUESCRIPT_DIR);
         this.globalConfigHandler.setRuntimeDir(GLOBAL_SETTINGS.RUNTIME_DIR);
     }
@@ -39,12 +63,12 @@ class UpdateHandler extends CommandHandler {
         if (esp32Config.idfVersion === GLOBAL_SETTINGS.ESP_IDF_VERSION) {
             throw new SkipStep('not needed', undefined);
         }
+        this.existingEspDir = esp32Config.rootDir;
 
-        fs.removeDir(esp32Config.rootDir);
+        fs.moveDir(this.existingEspDir, this.tmpEspDir);
         fs.makeDir(GLOBAL_SETTINGS.ESP_ROOT_DIR);
         await this.cloneEspIdf();
         await this.runEspIdfInstallScript();
-
         this.globalConfigHandler.updateBoardConfig('esp32', {
             idfVersion: GLOBAL_SETTINGS.ESP_IDF_VERSION,
             rootDir: GLOBAL_SETTINGS.ESP_ROOT_DIR,
--- cli/tests/commands/board/update.test.ts
+++ cli/tests/commands/board/update.test.ts
@@ -1,6 +1,8 @@
 import { handleUpdateCommand } from '../../../src/commands/board/update';
-import { deleteGlobalEnv, DUMMY_ESP_IDF_VERSION, getGlobalConfig, setupDefaultGlobalEnv, setupGlobalEnvWithEsp32, DUMMY_VM_VERSION, spyGlobalSettings } from '../global-env-helper';
+import { deleteGlobalEnv, DUMMY_ESP_IDF_VERSION, getGlobalConfig, setupDefaultGlobalEnv, setupGlobalEnvWithEsp32, DUMMY_VM_VERSION, spyGlobalSettings, DUMMY_OLD_VM_VERSION, DUMMY_OLD_ESP_IDF_VERSION } from '../global-env-helper';
 import { mockedDownloadAndUnzip, mockedExec } from '../mock-helpers';
+import * as fs from '../../../src/core/fs';
+import { GLOBAL_SETTINGS } from '../../../src/config/constants';
 
 
 describe('board update command', () => {
@@ -21,7 +23,7 @@ describe('board update command', () => {
                 return 'xtensa-esp-elf/bin';
             }
             return '';
-        })
+        });
 
         // --- Act ---
         await handleUpdateCommand();
@@ -56,5 +58,52 @@ describe('board update command', () => {
         expect(mockedDownloadAndUnzip).toHaveBeenCalledTimes(1);
         expect(mockedExec).not.toHaveBeenCalledWith(expect.stringContaining('git clone'),expect.any(Object));
         expect(getGlobalConfig().version).toMatch(DUMMY_VM_VERSION);
+    });
+
+    it('should restore old runtime if error occures during downloading the new runtime', async () => {
+        // --- Arrange ---
+        setupGlobalEnvWithEsp32(true, true);
+        mockedExec.mockImplementation((command: string) => {
+            if (command.endsWith('which xtensa-esp32-elf-gcc')) {
+                return 'xtensa-esp-elf/bin';
+            }
+            return '';
+        });
+        mockedDownloadAndUnzip.mockImplementation(() => {
+            throw new Error('Failed to download.');
+        })
+
+        // --- Act ---
+        await handleUpdateCommand();
+
+        // --- Assert ---
+        expect(mockedDownloadAndUnzip).toHaveBeenCalledTimes(1);
+        expect(mockedExec).not.toHaveBeenCalledWith(expect.stringContaining('git clone'),expect.any(Object));
+        expect(mockedExec).not.toHaveBeenCalledWith(expect.stringContaining('install'));
+        expect(fs.exists(GLOBAL_SETTINGS.RUNTIME_DIR)).toBe(true);
+        expect(getGlobalConfig().version).toMatch(DUMMY_OLD_VM_VERSION);
+    });
+
+    it('should restore old runtime and esp dir if error occures during updating esp-idf', async () => {
+        // --- Arrange ---
+        setupGlobalEnvWithEsp32(true, true);
+        mockedExec.mockImplementation((command: string) => {
+            if (command.startsWith('git clone')) {
+                throw new Error('Failed to cloning ESP-IDF');
+            }
+            return '';
+        });
+
+        // --- Act ---
+        await handleUpdateCommand();
+
+        // --- Assert ---
+        expect(mockedDownloadAndUnzip).toHaveBeenCalledTimes(1);
+        expect(mockedExec).not.toHaveBeenCalledWith(expect.stringContaining('git clone'),expect.any(Object));
+        expect(mockedExec).not.toHaveBeenCalledWith(expect.stringContaining('install'));
+        expect(fs.exists(GLOBAL_SETTINGS.RUNTIME_DIR)).toBe(true);
+        expect(fs.exists(GLOBAL_SETTINGS.ESP_ROOT_DIR)).toBe(true);
+        expect(getGlobalConfig().version).toMatch(DUMMY_OLD_VM_VERSION);
+        expect(getGlobalConfig().boards.esp32.idfVersion).toMatch(DUMMY_OLD_ESP_IDF_VERSION);
     });
 });
